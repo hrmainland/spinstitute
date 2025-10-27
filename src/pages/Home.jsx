271b43192bdf13dfb648f17da922eb2122c75a4f
jhmainland <jhmainland@gmail.com>
2025-10-27 17:02:13 +1100

    Update Home delete call to action.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Box } from '@mui/material';
 import Hero from '../components/Hero';
 import MissionStatement from '../components/StatsSection';
-import CallToAction from '../components/CallToAction';
 
 export default function Home() {
   return (
     <Box>
       <Hero />
       <MissionStatement />
-      <CallToAction />
     </Box>
   );
 }
