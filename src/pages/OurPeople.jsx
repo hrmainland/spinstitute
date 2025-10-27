cb8991e01b35c831d3db18942cf93ff812798397
jhmainland <jhmainland@gmail.com>
2025-10-27 15:32:53 +1100

    Update OurPeople test new image

--- src/pages/OurPeople.jsx
+++ src/pages/OurPeople.jsx
@@ -7,7 +7,7 @@
     py: { xs: 8, md: 10 },
     color: 'white',
     backgroundImage:
-      'url("https://www.signatureluxurytravel.com.au/wp-content/uploads/2023/03/DGC_Aerial20_00040_edit.jpg")',
+      'url("https://ballrealty.com.au/sites/ballrealty/media/blog/21805317085_b9f34e59a9_o.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
@@ -26,38 +26,3 @@
   />
 
   {/* Header content */}
-  <Box sx={{ position: 'relative', zIndex: 2 }}>
-    <Box
-      sx={{
-        width: '80px',
-        height: '4px',
-        backgroundColor: '#D2C1B6',
-        borderRadius: '2px',
-        mx: 'auto',
-        mb: 4,
-      }}
-    />
-    <Typography
-      variant="h2"
-      component="h1"
-      sx={{
-        mb: 3,
-        fontSize: { xs: '2.5rem', sm: '3rem', md: '3.5rem' },
-      }}
-    >
-      Our People
-    </Typography>
-    <Typography
-      variant="body1"
-      sx={{
-        maxWidth: '700px',
-        mx: 'auto',
-        fontSize: { xs: '1.1rem', md: '1.2rem' },
-        lineHeight: 1.8,
-      }}
-    >
-      Our people are embedded in existing political parties, academia, the bureaucracy,
-      commerce and the law. We have networks around the country and regularly host events in the Gold Coast, Sydney and Canberra. 
-    </Typography>
-  </Box>
-</Box>
